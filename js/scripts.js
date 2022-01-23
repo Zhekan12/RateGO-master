89e2e732245dbbf1ff3db626fe4c5c45d0e43ba6
Zhenya Zakharchuk <ivvetto.007@gmail.com>
2022-01-23 23:44:45 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    // owl-carusel
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:100,
+        nav:true,
+        autoplay:true,
+        navText: [ '', ' ' ],
+        responsive:{
+            320:{
+                items:1
+            },
+
+            767:{
+                items:2
+            }
+        }
+    });
+
+});
+
+const routesBlock = document.querySelector('.routes-block');
+routesBlock.addEventListener('click',function(event){
+    let currentTarget = event.target;
+    if (currentTarget.classList.contains('button-click-red')) {
+        currentTarget = currentTarget.parentNode;
+    }
+    if (currentTarget.classList.contains('button-click')){
+        const blocks = document.querySelectorAll('.block');
+        blocks.forEach(function(block){
+            block.classList.remove('active');
+        })
+        currentTarget.parentNode.classList.add('active')
+        console.log(currentTarget.dataset.truckClassname);
+        const truck = document.querySelector('.truck');
+        truck.classList.remove('move-1', 'move-2', 'move-3', 'move-4');
+        truck.classList.add(currentTarget.dataset.truckClassname)
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
